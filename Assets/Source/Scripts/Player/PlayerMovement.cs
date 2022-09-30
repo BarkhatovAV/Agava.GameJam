3abf5f0fb98814eec123685cefcc9a1feb12fb4e
redheadzues <48310838+redheadzues@users.noreply.github.com>
2022-09-30 22:14:14 +0300

    Create PlayerMovement script

---
+++ Assets/Source/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            MoveDirection(Vector3.forward);
+        if(Input.GetKey(KeyCode.S))
+            MoveDirection(-Vector3.forward);
+        if (Input.GetKey(KeyCode.D))
+            MoveDirection(Vector3.right);
+        if (Input.GetKey(KeyCode.A))
+            MoveDirection(-Vector3.right);
+    }
+
+    private void MoveDirection(Vector3 direction)
+    {
+        transform.position = transform.position + direction * _speed * Time.deltaTime;
+    }
+}
