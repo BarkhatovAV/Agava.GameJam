7d22edab2b990b6340a566702052736808dade6d
VitalyKorzin <72571981+VitalyKorzin@users.noreply.github.com>
2022-10-01 18:29:11 +0300

    Create enemy animator class

---
+++ Assets/Source/Scripts/Enemy/EnemyAnimator.cs
@@ -0,0 +1,9 @@
+public static class EnemyAnimator
+{
+    public static class Params
+    {
+        public const string IsAttacking = nameof(IsAttacking);
+        public const string IsRunning = nameof(IsRunning);
+        public const string IsDying = nameof(IsDying);
+    }
+}
