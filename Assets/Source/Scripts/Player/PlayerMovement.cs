30c079df9c0768ef8b4debd0ff01670744a63eb2
redheadzues <48310838+redheadzues@users.noreply.github.com>
2022-10-02 02:29:10 +0300

    Fixed ghost bug in PlayerMover
    transform.position changed on rigidbody.velocity

--- Assets/Source/Scripts/Player/PlayerMovement.cs
+++ Assets/Source/Scripts/Player/PlayerMovement.cs
@@ -1,25 +1,38 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _speed;
 
+    private Rigidbody _rigidbody;
+    private Vector3 _direction;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
     private void Update()
     {
+        _direction = Vector3.zero;
+
         if (Input.GetKey(KeyCode.W))
-            MoveDirection(Vector3.forward);
+            _direction += Vector3.forward;
         if (Input.GetKey(KeyCode.S))
-            MoveDirection(-Vector3.forward);
-        if(Input.GetKey(KeyCode.D))
-            MoveDirection(Vector3.right);
-        if(Input.GetKey(KeyCode.A))
-            MoveDirection(-Vector3.right);
+            _direction -= Vector3.forward;
+        if (Input.GetKey(KeyCode.D))
+            _direction += Vector3.right;
+        if (Input.GetKey(KeyCode.A))
+            _direction -= Vector3.right;
+
+        MoveDirection(_direction);
     }
 
     private void MoveDirection(Vector3 direction)
     {
-        Vector3 move = Quaternion.Euler(0, transform.eulerAngles.y, 0) * direction;
+        Vector3 move = Quaternion.Euler(0, transform.eulerAngles.y, 0) * direction * _speed * Time.deltaTime;
 
-        transform.position += move * _speed * Time.deltaTime;
+        _rigidbody.velocity =  new Vector3(move.x, _rigidbody.velocity.y, move.z);
     }
 }
