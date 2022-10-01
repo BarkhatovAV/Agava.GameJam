97545ff6c18dd6399f7bfd7156f52318b627a900
redheadzues <48310838+redheadzues@users.noreply.github.com>
2022-10-01 18:47:27 +0300

    Realize interface Iweapon on ChainSaw weapon

--- Assets/Source/Scripts/Player/Armory/ChainSaw.cs
+++ Assets/Source/Scripts/Player/Armory/ChainSaw.cs
@@ -2,6 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChainSaw : Weapon
+public class ChainSaw : Weapon, IWeapon
 {
+    public void Fire(RaycastHit hitInfo)
+    {
+        if (hitInfo.collider.TryGetComponent(out Ground ground) && _shotDistance > hitInfo.distance)
+            print(hitInfo.distance);
+    }
 }
