29c8583245b0445620724e599340d38b8ed9076d
VitalyKorzin <72571981+VitalyKorzin@users.noreply.github.com>
2022-10-01 10:45:13 +0300

    Create death logic

--- Assets/Source/Scripts/Enemy/Enemy.cs
+++ Assets/Source/Scripts/Enemy/Enemy.cs
@@ -3,17 +3,21 @@ using System;
 using UnityEngine;
 
 [RequireComponent(typeof(EnemyMover), typeof(EnemyAttacker), typeof(EnemyHealth))]
+[RequireComponent(typeof(BehaviorTree))]
 public class Enemy : MonoBehaviour
 {
     private EnemyMover _mover;
     private EnemyAttacker _attacker;
+    private BehaviorTree _behaviorTree;
     private EnemyHealth _health;
 
     private void Awake()
     {
         _mover = GetComponent<EnemyMover>();
         _attacker = GetComponent<EnemyAttacker>();
+        _behaviorTree = GetComponent<BehaviorTree>();
         _health = GetComponent<EnemyHealth>();
+        _health.Ended += OnHealthEnded;
     }
 
     public void Initialize(ITarget target)
@@ -25,9 +29,16 @@ public class Enemy : MonoBehaviour
         _attacker.Initialize(target);
     }
 
-
     public void Apply(int damage)
     {
         _health.Apply(damage);
+    }
+
+    private void OnHealthEnded()
+    {
+        _health.Ended -= OnHealthEnded;
+        _behaviorTree.enabled = false;
+        _mover.StopMoving();
+        _attacker.StopAttack();
     }
 }
--- Assets/Source/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Source/Scripts/Enemy/EnemyHealth.cs
@@ -1,10 +1,16 @@
 using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EnemyHealth : MonoBehaviour
 {
     [Min(0)]
     [SerializeField] private int _value;
+    [Min(0)]
+    [SerializeField] private float _delayBeforeDeath;
+
+    public event UnityAction Ended;
 
     public void Apply(int damage)
     {
@@ -12,5 +18,15 @@ public class EnemyHealth : MonoBehaviour
             throw new ArgumentOutOfRangeException(nameof(damage));
 
         _value -= damage;
+
+        if (_value <= 0)
+            StartCoroutine(Die());
+    }
+
+    private IEnumerator Die()
+    {
+        Ended?.Invoke();
+        yield return new WaitForSeconds(_delayBeforeDeath);
+        Destroy(gameObject);
     }
 }
