1fb3ae0587d9f070a7ae84fbd5f49c60e99bf336
redheadzues <48310838+redheadzues@users.noreply.github.com>
2022-10-01 19:23:26 +0300

    Realize interface IWeapon and add BulletsArmory in FlameThrower

--- Assets/Source/Scripts/Player/Armory/FlameThrower.cs
+++ Assets/Source/Scripts/Player/Armory/FlameThrower.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class FlameThrower : Weapon, IWeapon
 {
+    private BulletsArmory _bullets = new BulletsArmory();
+
+    public override void Fire(RaycastHit hitInfo)
+    {
+        if(_bullets.Value > 0)
+        {
+            _bullets.ShotBullet();
+            base.Fire(hitInfo);
+        }
+    }
+
+    public void TakeBullets(int value)
+    {
+        _bullets.AddBullets(value);
+    }
 
 }
