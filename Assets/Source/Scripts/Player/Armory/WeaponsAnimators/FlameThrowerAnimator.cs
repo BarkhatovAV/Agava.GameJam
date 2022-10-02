f36e4e3f2a5c312a5e1a90fa1c751e63a6c7b18e
redheadzues <48310838+redheadzues@users.noreply.github.com>
2022-10-02 03:56:06 +0300

    add audio on flamethrower

--- Assets/Source/Scripts/Player/Armory/WeaponsAnimators/FlameThrowerAnimator.cs
+++ Assets/Source/Scripts/Player/Armory/WeaponsAnimators/FlameThrowerAnimator.cs
@@ -2,22 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class FlameThrowerAnimator : WeaponAnimator
 {
     [SerializeField] private ParticleSystem _particleFlame;
 
+    private AudioSource _audioEffect;
+
     private void Awake()
     {
+        _audioEffect = GetComponent<AudioSource>();
         _particleFlame.Stop();
     }
 
     protected override void StartAnimation()
     {
         _particleFlame.Play();
+        _audioEffect.Play();
     }
 
     protected override void StopAnimation()
     {
         _particleFlame.Stop();
+        _audioEffect.Stop();
     }
 }
