561708681e7e9439d07684213c9ed17337f3c14f
redheadzues <48310838+redheadzues@users.noreply.github.com>
2022-10-02 21:27:30 +0300

    Add change weapon on mouse wheel

--- Assets/Source/Scripts/Player/Armory/WeaponArmory.cs
+++ Assets/Source/Scripts/Player/Armory/WeaponArmory.cs
@@ -25,6 +25,24 @@ public class WeaponArmory : MonoBehaviour
         return _weapons[_currentWeaponIndex];
     }
 
+    public Weapon ChangeWeapon(int value)
+    {       
+        if (value > 0)
+            return ChangeWeapon();
+
+        DisplayWeapon();
+
+        if (_currentWeaponIndex + value >= 0)
+            _currentWeaponIndex += value;
+        else
+            _currentWeaponIndex = _weapons.Count - 1;
+
+        WeaponChanged?.Invoke(_currentWeaponIndex);
+        DisplayWeapon();
+
+        return _weapons[_currentWeaponIndex];
+    }
+
     private void DisplayWeapon()
     {
         if (_weapons[_currentWeaponIndex].gameObject.activeSelf == true)
