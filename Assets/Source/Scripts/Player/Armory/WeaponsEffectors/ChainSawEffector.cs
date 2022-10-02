3fd3cf0ee56db10c769e7370599f840e86b72ebc
redheadzues <48310838+redheadzues@users.noreply.github.com>
2022-10-02 12:35:38 +0300

    Add blood splat effect on chainsaw

--- Assets/Source/Scripts/Player/Armory/WeaponsEffectors/ChainSawEffector.cs
+++ Assets/Source/Scripts/Player/Armory/WeaponsEffectors/ChainSawEffector.cs
@@ -2,12 +2,15 @@ using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(Collider))]
 public class ChainSawEffector : WeaponEffector
 {
     [SerializeField] private AudioClip _audioSawIdle;
     [SerializeField] private AudioClip _audioSawAttack;
+    [SerializeField] private ParticleSystem _particleBlood;
 
-    AudioSource _audioEffect;
+    private AudioSource _audioEffect;
+    private Collider _collider;
     
 
     private Animator _animator;
@@ -18,29 +21,41 @@ public class ChainSawEffector : WeaponEffector
     {
         _animator = GetComponent<Animator>();
         _audioEffect = GetComponent<AudioSource>();
+        _collider = GetComponent<Collider>();
     }
 
     private void OnEnable()
     {
         _animator.Play(c_Idle);
         ChangeAudioEffect(_audioSawIdle);
+        _particleBlood.Stop();
+        _collider.enabled = false;
     }
 
     protected override void StartPlayEffects()
     {
         _animator.Play(c_ShakeSaw);
         ChangeAudioEffect(_audioSawAttack);
+        _collider.enabled = true;
     }
 
     protected override void StopPlayEffects()
     {
         ChangeAudioEffect(_audioSawIdle);
         _animator.Play(c_Idle);
+        _collider.enabled = false;
+        _particleBlood.Stop();
     }
 
     private void ChangeAudioEffect(AudioClip audio)
     {
         _audioEffect.clip = audio;
         _audioEffect.Play();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.TryGetComponent(out Ground ground))
+            _particleBlood.Play();
     }
 }
